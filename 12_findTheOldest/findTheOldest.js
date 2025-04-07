5e31da9eb6dd2e2e06ff596ce6ee6ed3736fbf63
gkuczynski <xgkucz@gmail.com>
2025-04-07 13:36:03 +0200

    Solved 12_findTheOldest task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const currentYear = new Date().getFullYear();
 
+  const years = arr
+  .map((person) => person.yearOfDeath ? person.age = person.yearOfDeath - person.yearOfBirth: person.age = currentYear - person.yearOfBirth)
+  
+  let oldest = arr[0];
+  arr.filter((person) => person.age > oldest.age ? (oldest.age = person.age, oldest.name = person.name) : oldest.age = oldest.age)
+  
+  return oldest
 };
 
 // Do not edit below this line
