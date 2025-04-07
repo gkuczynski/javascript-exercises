3ef78643e25e063c0c73518ae6396378ea356f9b
gkuczynski <xgkucz@gmail.com>
2025-04-07 12:43:22 +0200

    Solved 09_palindromes task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let cleanedUp = str.split("");
+  const chars = "1234567890qwertyuiopasdfghjklzxcvbnm";
 
+  cleanedUp = cleanedUp
+    .map((letter) => letter.toLowerCase())
+    .filter((letter) => chars.includes(letter))
+    .join("");
+
+  return cleanedUp === cleanedUp.split("").reverse().join("");
 };
 
 // Do not edit below this line
